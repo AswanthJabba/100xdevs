1509118b75c91c5e745535b20070ce279ed951a3
AswanthJabba <jabbaaswanth@gmail.com>
2023-12-10 01:18:42 +0530

    DRY tweaks to anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -3,6 +3,20 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
+
+function buildCharFrequencyDict(str) {
+  const dict = {};
+  str = str.toLowerCase().trim();
+  for (const element of str) {
+      if (element in dict) {
+          dict[element] += 1;
+      } else {
+          dict[element] = 1;
+      }
+  }
+  return dict;
+}
+
 function isAnagram(str1, str2) {
 
   // if(str1.length!==str2.length){
@@ -15,47 +29,23 @@ function isAnagram(str1, str2) {
   //   return false
   // }
 
+  const dict1 = buildCharFrequencyDict(str1);
+  const dict2 = buildCharFrequencyDict(str2);
 
-  let dict1 = {};
-  let dict2 = {};
-  str1 = str1.toLowerCase().trim();
-  str2 = str2.toLowerCase().trim();
-  for(const element of str1){
-    if(element in dict1){
-      dict1[element] += 1;
-    }
-    else{
-      dict1[element] = 1;
-    }
-  }
-  for(const element of str2){
-    if(element in dict2){
-      dict2[element] += 1;
-    }
-    else{
-      dict2[element] = 1;
-    }
-  }
-  for(const key in dict1){
-    if(dict2.hasOwnProperty(key) && dict1[key] == dict2[key]){
-      continue;
-    }
-    else{
-      return false
-    }
+  for (const key in dict1) {
+      if (!(key in dict2) || dict1[key] !== dict2[key]) {
+          return false;
+      }
   }
 
-  for(const key in dict2){
-    if(dict1.hasOwnProperty(key) && dict1[key] == dict2[key]){
-      continue;
-    }
-    else{
-      return false
-    }
+  for (const key in dict2) {
+      if (!(key in dict1) || dict1[key] !== dict2[key]) {
+          return false;
+      }
   }
 
-  return true
-  
+  return true;
 }
+
 
 module.exports = isAnagram;
